fb7ca0f9a028a2e1c189f682ba7133956207415d
Ororura <hitmanlol890@gmail.com>
2025-03-02 15:29:10 +0300

    add middleware for logging, recovery, and CORS; implement router for user routes

---
+++ internal/platform/http/middleware/middleware.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func Logger() gin.HandlerFunc {
+	return gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	})
+}
+
+func Recovery() gin.HandlerFunc {
+	return gin.Recovery()
+}
+
+func Cors() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Content-Length"},
+		AllowCredentials: true,
+	})
+}
---
+++ internal/platform/http/router/router.go
@@ -0,0 +1,32 @@
+package router
+
+import (
+	"bazar/internal/platform/http/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Router struct {
+	engine      *gin.Engine
+	userHandler *handlers.UserHandler
+}
+
+func NewRouter(engine *gin.Engine, userHandler *handlers.UserHandler) *Router {
+	return &Router{engine: engine, userHandler: userHandler}
+}
+
+func (r *Router) RegisterRoutes() {
+	api := r.engine.Group("/api/v1")
+	{
+		users := api.Group("/users")
+		{
+			users.POST("/", r.userHandler.CreateUser)
+			users.GET("/", r.userHandler.GetAllUsers)
+			users.GET("/:id", r.userHandler.GetUserById)
+		}
+	}
+}
+
+func (r *Router) Run(addr string) error {
+	return r.engine.Run(addr)
+}
