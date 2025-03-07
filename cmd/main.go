7fdc66c730885c14425f6dedd5a843e3461896ff
Ororura <hitmanlol890@gmail.com>
2025-03-07 12:14:26 +0300

    feat: refactor main function to improve structure and remove bootstrap function; update NFT creation query

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,45 @@
 package main
 
 import (
-	"bazar/internal/app"
+	"bazar/config"
+	"bazar/internal/app/services"
+	"bazar/internal/platform/database/repositories"
+	"bazar/internal/platform/http/handlers"
+	"bazar/internal/platform/http/router"
 	"fmt"
+	"github.com/jmoiron/sqlx"
 	"log"
 )
 
 func main() {
-	application, err := app.BootStrap()
+	cfg := config.LoadConfig()
+	db, err := config.ConnectDB(cfg)
+
 	if err != nil {
-		log.Fatalf("Failed to bootstrap application: %v", err)
+		log.Fatalf("Error connecting to database: %v", err)
 	}
 
-	defer func() {
-		if err := application.DB.Close(); err != nil {
+	defer func(db *sqlx.DB) {
+		err := db.Close()
+		if err != nil {
 			fmt.Printf("Error closing database connection: %v", err)
 		}
-	}()
+	}(db)
 
-	if err := application.Router.Run(":8080"); err != nil {
+	log.Println("Success connection!")
+
+	userRepo := repositories.NewUserRepository(db)
+	userService := services.NewUserRepository(userRepo)
+	userHandler := handlers.NewUserHandler(userService)
+
+	nftRepo := repositories.NewNFTRepository(db)
+	nftService := services.NewNFTService(nftRepo)
+	nftHandler := handlers.NewNFTHandler(nftService)
+
+	newRouter := router.NewRouter(userHandler, nftHandler)
+	newRouter.RegisterRoutes()
+
+	if err := newRouter.Run(":8080"); err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
 }
