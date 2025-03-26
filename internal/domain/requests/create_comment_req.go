5497c002be36e2c39942d27ae9d3b2a40612a216
Ororura <hitmanlol890@gmail.com>
2025-03-26 21:31:16 +0300

    feat(comment): add fields to CreateCommentReq for token ID, owner, and content

--- internal/domain/requests/create_comment_req.go
+++ internal/domain/requests/create_comment_req.go
@@ -1,5 +1,7 @@
 package requests
 
-// TODO implement me
 type CreateCommentReq struct {
+	TokenId      uint   `json:"token_id"`
+	OwnerComment uint   `json:"owner_comment"`
+	Content      string `json:"content"`
 }
