2b919e0803abf2cceacfad92a8503b646dad5606
Ororura <hitmanlol890@gmail.com>
2025-04-29 17:38:19 +0300

    refactor: clean up SQL table creation and improve token URI generation function

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,14 +1,12 @@
-CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY,
-    eth_address VARCHAR(42) UNIQUE NOT NULL,
-    avatar_url VARCHAR(255) DEFAULT 'uploads/avatars/default-avatar.jpg'
-);
+CREATE TABLE
+    users (
+        id BIGSERIAL PRIMARY KEY,
+        eth_address VARCHAR(42) UNIQUE NOT NULL,
+        avatar_url VARCHAR(255) DEFAULT 'uploads/avatars/default-avatar.jpg'
+    );
 
 INSERT INTO
     users (eth_address)
-VALUES (
-        '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
-    ),
-    (
-        '0x70997970C51812dc3A010C7d01b50e0d17dc79C8'
-    );
+VALUES
+    ('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'),
+    ('0x70997970C51812dc3A010C7d01b50e0d17dc79C8');
--- pkg/utils/tokenURI.go
+++ pkg/utils/tokenURI.go
@@ -1,7 +1,12 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
 
-func GenerateTokenURI(baseUrl string, image string) string {
-	return fmt.Sprintf("%s/metadata/%s", baseUrl, image)
+	"github.com/google/uuid"
+)
+
+func GenerateTokenURI(baseUrl string) string {
+	id := uuid.New().String() // например, "3e4666bf-d5e5-4aa7-b8ce-cefe41c7568a"
+	return fmt.Sprintf("%s/metadata/%s", baseUrl, id)
 }
